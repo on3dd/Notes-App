7e089696772872d7465976ccc604befbe6a6e084
Artem <onedeadwave@gmail.com>
2019-10-30 21:03:08 +1000

    Started creating a upload page

---
+++ client/components/UploadForm.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="title"
+      :rules="titleRules"
+      label="Название"
+      required
+    ></v-text-field>
+
+    <v-textarea
+      v-model="description"
+      :rules="descriptionRules"
+      label="Описание"
+      hint="Введите описание работы"
+    ></v-textarea>
+
+    <v-text-field
+      v-model="category"
+      :rules="categoryRules"
+      label="Категория"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="subject"
+      :rules="subjectRules"
+      label="Предмет"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="teacher"
+      :rules="teacherRules"
+      label="Преподаватель"
+      required
+    ></v-text-field>
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'Вы должны принять условия пользования!']"
+      label="Я согласен с условиями пользования сервисом"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      color="success"
+      class="mr-4"
+    >
+      Отправить
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+    export default {
+        name: "UploadForm",
+        data: () => ({
+            valid: true,
+            title: '',
+            titleRules: [
+                v => !!v || 'Поле быть заполнено',
+                v => (v && v.length >= 10) || 'Название должно содержать как минимум 10 символов',
+            ],
+
+            category: '',
+            categoryRules: [
+                v => !!v || 'Поле быть заполнено',
+            ],
+
+            subject: '',
+            subjectRules: [
+                v => !!v || 'Поле быть заполнено',
+            ],
+
+            teacher: '',
+            teacherRules: [
+                v => !!v || 'Поле быть заполнено',
+            ],
+
+            checkbox: false,
+        }),
+
+        methods: {
+            validate () {
+                if (this.$refs.form.validate()) {
+                    this.snackbar = true
+                }
+            },
+            reset () {
+                this.$refs.form.reset()
+            },
+            resetValidation () {
+                this.$refs.form.resetValidation()
+            },
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/upload.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar>
+      <Navbar></Navbar>
+    </v-app-bar>
+    <v-content>
+      <v-container
+        class="my-12"
+      >
+        <v-row
+          justify="center"
+          no-gutters
+          class="mx-md-6"
+        >
+          <v-col
+            :sm="10"
+            :md="6"
+            align="center"
+          >
+            <span class="display-3 text-center">Загрузка работы</span>
+            <UploadForm></UploadForm>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+    import Navbar from "../components/Navbar";
+    import UploadForm from "../components/UploadForm";
+    import Footer from "../components/Footer";
+
+    export default {
+        components: {
+            Navbar,
+            UploadForm,
+            Footer
+        },
+        name: "upload.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>

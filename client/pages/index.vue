f83169fc761e10e18634de8db0d9b8f68324712a
Artem <onedeadwave@gmail.com>
2019-10-30 13:57:31 +1000

    Fixed bug with vuetify styles

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,73 +1,69 @@
 <template>
-  <div>
+  <v-app id="inspire">
     <Navbar></Navbar>
-    <section class="container">
-      <div>
-        <app-logo/>
-        <h1 class="title">
-          starter
-        </h1>
-        <h2 class="subtitle">
-          Client for application
-        </h2>
-        <div class="links">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            class="button--green">Documentation</a>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            class="button--grey">GitHub</a>
-        </div>
-      </div>
-    </section>
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  :href="source"
+                  icon
+                  large
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  large
+                  href="https://codepen.io/johnjleider/pen/zgxeLQ"
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-codepen</v-icon>
+                </v-btn>
+              </template>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
     <Footer></Footer>
-  </div>
+  </v-app>
 </template>
 
 <script>
-    import AppLogo from '~/components/AppLogo.vue'
-    import Navbar from "~/components/Navbar"
-    import Footer from "~/components/Footer";
+    import Navbar from "../components/Navbar";
+    import Footer from "../components/Footer";
 
     export default {
         components: {
-            AppLogo,
             Navbar,
-            Footer
-        }
+            Footer,
+        },
+        props: {
+            source: String,
+        },
+
+        data: () => ({
+            drawer: null,
+        }),
     }
 </script>
-
-<style>
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
-  }
-</style>
-

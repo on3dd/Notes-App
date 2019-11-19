79ee8f39c258d5337b0f5700aa8a713a166def42
gettmure <myshalov.re@students.dvfu.ru>
2019-11-19 14:26:57 +1000

    added login page

---
+++ client/pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app id="inspire">
+    
+    <v-content style="min-height: 100vh;">
+        
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+        </v-row>
+      </v-container>
+    </v-content>
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+    import Footer from "~/components/Footer";
+
+    export default {
+        components: {
+            Footer,
+        },
+        props: {
+            source: String,
+        },
+
+        data: () => ({
+            drawer: null,
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>

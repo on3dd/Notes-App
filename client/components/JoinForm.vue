e1a76073a665c5010d0eefdf733fefca30fb16b3
gettmure <myshalov.re@students.dvfu.ru>
2019-12-03 18:35:47 +1000

    added registration page

---
+++ client/components/JoinForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-form id="form" v-model="valid" lazy-validation onsubmit="return false;">
+    <span class="display-3 text-center d-block mb-3">Регистрация</span>
+    <v-text-field
+      v-model="username"
+      :rules="usernameRules"
+      label="Логин"
+      required
+      name="username"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      :rules="passwordRules"
+      label="Пароль"
+      required
+      name="password"
+      type="password"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="repeatPassword"
+      :rules="repeatPasswordRules"
+      label="Повторите пароль"
+      required
+      name="repeatPassword"
+      type="repeatPassword"
+    ></v-text-field>
+
+    <v-btn 
+      x-large color="success" 
+      class="mx-4" 
+      :disabled="!valid" 
+      type="submit" 
+      @click="() => {}"
+      >
+        Зарегистрироваться
+      </v-btn>
+  </v-form>
+</template>
+
+<script>
+    export default {
+        name: "JoinForm",
+        data: () => ({
+            valid: false,
+            username: '',
+            usernameRules: [
+                v => !!v || "Поле должно быть заполнено"
+            ],
+
+            password: '',
+            passwordRules: [
+                v => !!v || "Поле должно быть заполнено"
+            ],
+
+            repeatPassword: '',
+            repeatPasswordRules: [
+                v => !!v || "Поле должно быть заполнено"
+            ]
+        }),
+
+        methods: {
+
+        }
+    };
+</script>
+
+<style>
+  .v-input__slot {
+    margin-bottom: 0 !important;
+  }
+</style>
---
+++ client/pages/join.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app id="inspire">
+    <Navbar></Navbar>
+    <v-content style="min-height: 100vh">
+      <v-container class="my-12">
+        <v-row justify="center" no-gutters class="mx-md-6">
+          <v-col :sm="10" :md="6" align="center">
+            <JoinForm></JoinForm>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer";
+import JoinForm from "../components/JoinForm";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+    JoinForm
+  },
+  name: "join.vue"
+};
+</script>
+
+<style scoped>
+</style>
